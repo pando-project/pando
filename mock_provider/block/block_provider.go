c12316f269dc7671dae482d57e974fe6b38b9920
sssion <62473385+sssion@users.noreply.github.com>
2021-11-24 18:36:11 +0800

    add command subscribe provider

--- mock_provider/block/block_provider.go
+++ mock_provider/block/block_provider.go
@@ -46,6 +46,7 @@ func Store(bs blockstore.Blockstore, n ipld.Node) (ipld.Link, error) {
 	return lsys.Store(ipld.LinkContext{}, linkproto, n)
 }
 
+// eg: CAESQHWlReUYxW7FDvTAAqG+kNH2U7khW+iv0r+070+zKmFn9t80v5e30/NsBx5XzBLCE4uH/h3d3tpXlwCuO4YGN+w= 10 12D3KooWC3jxxw4TdQtoZDv3QNwmh9rtuiyVL8CADpnJYKHh9AiA /ip4/52.14.211.248/tcp/9000 30
 func main() {
 	if len(os.Args) < 5 {
 		fmt.Println("please input:\r\n1. provider private key\n2. mock block number\n3. Pando PeerID\n4. Pando MultiAddr\n5. Time wait for data transferring[optional, int]")
