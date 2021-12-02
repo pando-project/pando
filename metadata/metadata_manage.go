ac66e39c21d0a37f3cdfecd77c1ce7b00960188d
sssion <62473385+sssion@users.noreply.github.com>
2021-12-02 17:56:58 +0800

    add export car file in metamanager

--- metadata/metadata_manage.go
+++ metadata/metadata_manage.go
@@ -3,8 +3,10 @@ package metadata
 import (
 	"Pando/statetree/types"
 	"context"
+	"errors"
 	"fmt"
 	dag "github.com/ipfs/go-merkledag"
+	"path"
 
 	bsrv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
@@ -23,17 +25,33 @@ import (
 
 var log = logging.Logger("meta-manager")
 
-const SnapShotDuration = time.Second * 5
+var (
+	SnapShotDuration       = time.Second * 5
+	BackupMaxInterval      = time.Second * 3
+	BackupMaxDagNums       = 10000
+	BackupTmpDir           = "./tmp/"
+	BackFileName           = "backup.car"
+	BackupCheckNumInterval = time.Second * 60
+)
+
+var NoRecordBackup = errors.New("no records need backup")
 
 type MetaManager struct {
-	flushTime      time.Duration
-	recvCh         chan *MetaRecord
-	outStateTreeCh chan map[peer.ID]*types.ProviderState
-	ds             datastore.Datastore
-	bs             blockstore.Blockstore
-	dagds          format.NodeGetter
-	cache          map[peer.ID][]*MetaRecord
-	mutex          sync.Mutex
+	flushTime         time.Duration
+	recvCh            chan *MetaRecord
+	outStateTreeCh    chan map[peer.ID]*types.ProviderState
+	backupCh          chan cid.Cid
+	ds                datastore.Datastore
+	bs                blockstore.Blockstore
+	dagServ           format.NodeGetter
+	cache             map[peer.ID][]*MetaRecord
+	mutex             sync.Mutex
+	backupMaxInterval time.Duration
+}
+
+type backupRecord struct {
+	cid      cid.Cid
+	isBackup bool
 }
 
 type MetaRecord struct {
@@ -44,18 +62,20 @@ type MetaRecord struct {
 
 func New(ctx context.Context, ds datastore.Batching, bs blockstore.Blockstore) (*MetaManager, error) {
 	mm := &MetaManager{
-		flushTime:      SnapShotDuration,
-		recvCh:         make(chan *MetaRecord),
-		outStateTreeCh: make(chan map[peer.ID]*types.ProviderState),
-		ds:             ds,
-		bs:             bs,
-		cache:          make(map[peer.ID][]*MetaRecord),
-		dagds:          dag.NewDAGService(bsrv.New(bs, offline.Exchange(bs))),
+		flushTime:         SnapShotDuration,
+		recvCh:            make(chan *MetaRecord),
+		outStateTreeCh:    make(chan map[peer.ID]*types.ProviderState),
+		backupCh:          make(chan cid.Cid, 1000),
+		ds:                ds,
+		bs:                bs,
+		cache:             make(map[peer.ID][]*MetaRecord),
+		dagServ:           dag.NewDAGService(bsrv.New(bs, offline.Exchange(bs))),
+		backupMaxInterval: BackupMaxInterval,
 	}
 
 	go mm.dealReceivedMeta()
 	go mm.flushRegular()
-
+	go mm.backupDagToFilecoin(ctx)
 	return mm, nil
 }
 
@@ -67,6 +87,7 @@ func (mm *MetaManager) dealReceivedMeta() {
 				mm.cache[r.ProviderID] = make([]*MetaRecord, 0)
 			}
 			mm.cache[r.ProviderID] = append(mm.cache[r.ProviderID], r)
+			mm.backupCh <- r.Cid
 		}
 		mm.mutex.Unlock()
 	}
@@ -81,7 +102,6 @@ func (mm *MetaManager) flushRegular() {
 			for _, r := range records {
 				cidlist = append(cidlist, r.Cid)
 			}
-			//ExportMetaCar(mm.dagds, cidlist, "./received/"+peerID.String()[:5]+time.Now().String()+".car", mm.bs)
 			update[peerID] = &types.ProviderState{Cidlist: cidlist}
 		}
 		if len(update) > 0 {
@@ -103,8 +123,114 @@ func (mm *MetaManager) GetUpdateOut() <-chan map[peer.ID]*types.ProviderState {
 	return mm.outStateTreeCh
 }
 
-func ExportMetaCar(dagds format.NodeGetter, cidlist []cid.Cid, path string, bs blockstore.Blockstore) error {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+func (mm *MetaManager) backupDagToFilecoin(ctx context.Context) {
+	backupMutex := new(sync.Mutex)
+	waitBackupRecoed := make([]*backupRecord, 0)
+	t := time.NewTicker(time.Minute)
+	go func() {
+		for c := range mm.backupCh {
+			log.Debugw("received dag to backup")
+			waitBackupRecoed = append(waitBackupRecoed, &backupRecord{
+				cid:      c,
+				isBackup: false,
+			})
+			// delete record had been backup
+			select {
+			case _ = <-t.C:
+				for i, r := range waitBackupRecoed {
+					if r.isBackup {
+						waitBackupRecoed = append(waitBackupRecoed[:i], waitBackupRecoed[i+1:]...)
+					}
+				}
+			default:
+			}
+		}
+	}()
+
+	go func() {
+		for range time.NewTicker(mm.backupMaxInterval).C {
+			backupMutex.Lock()
+			log.Infow("start backup the car to filecoin")
+			// for update the isBackup later because the original slice has changed
+			_waitBackupRecoed := make([]*backupRecord, len(waitBackupRecoed))
+			copy(_waitBackupRecoed, waitBackupRecoed)
+			err := mm.backupRecordsAndUpdateStatus(ctx, _waitBackupRecoed)
+			if err != nil {
+				continue
+			}
+			log.Infow("finished backup the car to filecoin")
+			backupMutex.Unlock()
+		}
+	}()
+
+	go func() {
+		for range time.NewTicker(BackupCheckNumInterval).C {
+			backupMutex.Lock()
+			nums := 0
+			for _, r := range waitBackupRecoed {
+				if !r.isBackup {
+					nums++
+				}
+			}
+			if nums > BackupMaxDagNums {
+				_waitBackupRecoed := make([]*backupRecord, len(waitBackupRecoed))
+				copy(_waitBackupRecoed, waitBackupRecoed)
+				err := mm.backupRecordsAndUpdateStatus(ctx, _waitBackupRecoed)
+				if err != nil {
+					continue
+				}
+				log.Infow("finished backup the car to filecoin")
+			}
+			backupMutex.Unlock()
+		}
+	}()
+}
+
+func (mm *MetaManager) backupRecordsAndUpdateStatus(ctx context.Context, _waitBackupRecoed []*backupRecord) error {
+	waitBackupCidList := make([]cid.Cid, 0)
+	for _, r := range _waitBackupRecoed {
+		if !r.isBackup && r.cid != cid.Undef {
+			waitBackupCidList = append(waitBackupCidList, r.cid)
+		}
+	}
+	if len(waitBackupCidList) == 0 {
+		log.Infow(NoRecordBackup.Error())
+		return NoRecordBackup
+	}
+	err := ExportMetaCar(mm.dagServ, waitBackupCidList, BackupTmpDir+BackFileName, mm.bs)
+	if err != nil {
+		log.Errorf("failed to write Dags to car, err: %s", err.Error())
+		return err
+	}
+	err = mm.makeBackupDeal(ctx, BackupTmpDir+BackFileName)
+	if err != nil {
+		log.Errorf("failed to backup car to filecoin, err: %s", err.Error())
+		return err
+	}
+	for _, r := range _waitBackupRecoed {
+		r.isBackup = true
+	}
+	return nil
+}
+
+func (mm *MetaManager) makeBackupDeal(ctx context.Context, path string) error {
+	// todo
+	return nil
+}
+
+func ExportMetaCar(dagds format.NodeGetter, cidlist []cid.Cid, filepath string, bs blockstore.Blockstore) error {
+	_, err := os.Stat(path.Dir(filepath))
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir(path.Dir(filepath), os.ModePerm)
+			if err != nil {
+				log.Errorf("failed to create backup dir:%s , err:%s", path.Dir(filepath), err.Error())
+			}
+		} else {
+			log.Errorf("please input correct filepath, err : %s", err.Error())
+		}
+	}
+	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		log.Errorf("open file error : %s", err.Error())
 		return err
--- metadata/metadata_manage_test.go
+++ metadata/metadata_manage_test.go
@@ -4,42 +4,42 @@ import (
 	. "Pando/metadata"
 	"Pando/test/mock"
 	"context"
-	bsrv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	dag "github.com/ipfs/go-merkledag"
 	. "github.com/smartystreets/goconvey/convey"
 	"os"
 	"testing"
 	"time"
 )
 
-var (
-	testCid1, _ = cid.Decode("bafy2bzaceamp42wmmgr2g2ymg46euououzfyck7szknvfacqscohrvaikwfaa")
-	testCid2, _ = testCid1.Prefix().Sum([]byte("testdata2"))
-	testCid3, _ = testCid1.Prefix().Sum([]byte("testdata3"))
-)
-
 func TestReceiveRecordAndOutUpdate_(t *testing.T) {
 	Convey("test metadata manager", t, func() {
 		pando, err := mock.NewPandoMock()
 		So(err, ShouldBeNil)
-		mm, err := New(context.Background(), pando.DS, pando.BS)
-		So(err, ShouldBeNil)
-		mockRecord := []*MetaRecord{
-			{testCid1, "12D3KooWNtUworDmrdTUBrLqeD8s36MLnpRX1QJGQ46HXaJVBXV6", uint64(time.Now().UnixNano())},
-			{testCid2, "12D3KooWNtUworDmrdTUBrLqeD8s36MLnpRX1QJGQ46HXaJVBXV4", uint64(time.Now().UnixNano())},
-			{testCid3, "12D3KooWNtUworDmrdTUBrLqeD8s36MLnpRX1QJGQ46HXaJVBXV5", uint64(time.Now().UnixNano())},
-		}
 
-		Convey("test receive record and out update", func() {
+		Convey("test receive record, out update and backup because of maxInterval", func() {
+			mm, err := New(context.Background(), pando.DS, pando.BS)
+			So(err, ShouldBeNil)
+			provider, err := mock.NewMockProvider(pando)
+			So(err, ShouldBeNil)
+			err = pando.Core.Subscribe(context.Background(), provider.ID)
+			So(err, ShouldBeNil)
+			cidlist, err := provider.SendDag()
+			So(err, ShouldBeNil)
+			cidlist2, err := provider.SendDag()
+			So(err, ShouldBeNil)
+			cidlist3, err := provider.SendDag()
+			So(err, ShouldBeNil)
+			mockRecord := []*MetaRecord{
+				{cidlist[0], provider.ID, uint64(time.Now().UnixNano())},
+				{cidlist2[0], provider.ID, uint64(time.Now().UnixNano())},
+				{cidlist3[0], provider.ID, uint64(time.Now().UnixNano())},
+			}
 			recvCh := mm.GetMetaInCh()
 			for _, r := range mockRecord {
 				recvCh <- r
 			}
 			outCh := mm.GetUpdateOut()
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
 			t.Cleanup(func() {
 				cancel()
 			})
@@ -48,32 +48,58 @@ func TestReceiveRecordAndOutUpdate_(t *testing.T) {
 			case <-ctx.Done():
 				t.Error("timeout!not get update rightly")
 			case update := <-outCh:
-				So(len(update), ShouldEqual, 3)
+				So(len(update), ShouldEqual, 1)
 				So(update, ShouldContainKey, mockRecord[0].ProviderID)
-				So(update, ShouldContainKey, mockRecord[1].ProviderID)
-				So(update, ShouldContainKey, mockRecord[2].ProviderID)
-				So(update[mockRecord[0].ProviderID].Cidlist, ShouldResemble, []cid.Cid{testCid1})
-				So(update[mockRecord[1].ProviderID].Cidlist, ShouldResemble, []cid.Cid{testCid2})
-				So(update[mockRecord[2].ProviderID].Cidlist, ShouldResemble, []cid.Cid{testCid3})
+				So(update[mockRecord[0].ProviderID].Cidlist, ShouldResemble, []cid.Cid{mockRecord[0].Cid, mockRecord[1].Cid, mockRecord[2].Cid})
+				data, err := os.ReadFile(BackupTmpDir + BackFileName)
+				So(err, ShouldBeNil)
+				So(data, ShouldNotBeNil)
 			}
 		})
-		Convey("test export car", func() {
-			dags := dag.NewDAGService(bsrv.New(pando.BS, offline.Exchange(pando.BS)))
-			So(dags, ShouldNotBeNil)
+		Convey("test receive record, out update and backup because of maxDagNum", func() {
+			BackupMaxInterval = time.Second * 60
+			BackupCheckNumInterval = time.Second
+			BackupMaxDagNums = 1
+			mm, err := New(context.Background(), pando.DS, pando.BS)
+			So(err, ShouldBeNil)
 			provider, err := mock.NewMockProvider(pando)
 			So(err, ShouldBeNil)
 			err = pando.Core.Subscribe(context.Background(), provider.ID)
 			So(err, ShouldBeNil)
-			daglist, err := provider.SendDag()
+			cidlist, err := provider.SendDag()
 			So(err, ShouldBeNil)
-			time.Sleep(time.Second * 5)
-			tmpdir := t.TempDir()
-			carpath := tmpdir + time.Now().String() + ".car"
-			err = ExportMetaCar(dags, []cid.Cid{daglist[0]}, carpath, pando.BS)
+			cidlist2, err := provider.SendDag()
 			So(err, ShouldBeNil)
-			data, err := os.ReadFile(carpath)
+			cidlist3, err := provider.SendDag()
 			So(err, ShouldBeNil)
-			So(data, ShouldNotBeNil)
+			mockRecord := []*MetaRecord{
+				{cidlist[0], provider.ID, uint64(time.Now().UnixNano())},
+				{cidlist2[0], provider.ID, uint64(time.Now().UnixNano())},
+				{cidlist3[0], provider.ID, uint64(time.Now().UnixNano())},
+			}
+			//_ = logging.SetLogLevel("meta-manager", "debug")
+			recvCh := mm.GetMetaInCh()
+			for _, r := range mockRecord {
+				recvCh <- r
+			}
+			outCh := mm.GetUpdateOut()
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+			t.Cleanup(func() {
+				cancel()
+			})
+
+			select {
+			case <-ctx.Done():
+				t.Error("timeout!not get update rightly")
+			case update := <-outCh:
+				t.Log(update)
+				So(len(update), ShouldEqual, 1)
+				So(update, ShouldContainKey, mockRecord[0].ProviderID)
+				So(update[mockRecord[0].ProviderID].Cidlist, ShouldResemble, []cid.Cid{mockRecord[0].Cid, mockRecord[1].Cid, mockRecord[2].Cid})
+				data, err := os.ReadFile(BackupTmpDir + BackFileName)
+				So(err, ShouldBeNil)
+				So(data, ShouldNotBeNil)
+			}
 		})
 
 	})
