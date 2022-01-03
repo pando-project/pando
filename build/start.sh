74afd9a985d5a86836e58db042d1479e633b774d
Ben <xuqiang056@gmail.com>
2022-01-03 22:47:05 +0800

    add api doc

--- build/start.sh
+++ build/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -d "$HOME/.pando" ]; then
-  /opt/pando-server daemon
+  /opt/pando-server daemon && /opt/go-swagger serve -F=swagger /opt/swagger.yml -p 5000
 else
-  /opt/pando-server init && /opt/pando daemon
+  /opt/pando-server init && /opt/pando daemon && /opt/go-swagger serve -F=swagger /opt/swagger.yml -p 5000
 fi
