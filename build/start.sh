b84ca755bcca4049387d5e293b6b31ab8e71178b
Ben <xuqiang056@gmail.com>
2022-02-14 16:03:06 +0800

    modify start.sh to adapt volume mount

--- build/start.sh
+++ build/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -d "$HOME/.pando" ]; then
+if [ -d "$HOME/.pando/config.yaml" ]; then
   /opt/pando-server daemon
 else
   /opt/pando-server init && /opt/pando-server daemon
