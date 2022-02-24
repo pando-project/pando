603704e997d3dd172287ff2392b11713e542be8a
Ben <xuqiang056@gmail.com>
2022-02-24 14:46:00 +0800

    remove the logger does not exist

--- cmd/server/command/daemon.go
+++ cmd/server/command/daemon.go
@@ -93,10 +93,6 @@ func setLoglevel() error {
 		return err
 	}
 	logging.SetAllLoggers(logLevel)
-	err = logging.SetLogLevel("addrutil", "warn")
-	if err != nil {
-		return err
-	}
 	err = logging.SetLogLevel("basichost", "warn")
 	if err != nil {
 		return err
